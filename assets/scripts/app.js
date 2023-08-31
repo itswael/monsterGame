ef8bfa48b2436ea9b2b13f554367f5499d50ad6a
Mohd Wael <mohd.wael.cs.2016@miet.ac.in>
2023-09-01 00:17:20 +0530

    added strong attack logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -7,7 +8,14 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler(){
+function attackMonster(mode){
+    let maxDamage;
+    if(mode === 'ATTACK'){
+        maxDamage = ATTACK_VALUE;
+    }else{
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+
     const damage = dealMonsterDamage(ATTACK_VALUE);
     currentMonsterHealth -= damage;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
@@ -21,4 +29,13 @@ function attackHandler(){
     }
 }
 
-attackBtn.addEventListener('click', attackHandler); 
+function attackHandler(){
+    attackMonster('ATTACK');
+}
+
+function strongAttackHandler(){
+    attackMonster('STRONG_ATTACK');
+}
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler); 
